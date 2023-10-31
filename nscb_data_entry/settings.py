df88b1f5055747d0994c597074728098821d9d08
MAHESH ANANDE <maheshanande6@gmail.com>
2023-10-31 20:42:16 +0530

    change

--- nscb_data_entry/settings.py
+++ nscb_data_entry/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-o%%9_uwdoqo-gdzniq*3f*s57@nghrc(&ptr+fj@tidqd4+ff(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1",""]
+ALLOWED_HOSTS = ["127.0.0.1",".vercel.app"]
 
 
 # Application definition
