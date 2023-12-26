047b09617cd7c34aa0aa752bd972186c68932708
MAHESH ANANDE <maheshanande6@gmail.com>
2023-12-26 21:48:02 +0530

    up

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 echo "BUILD START"
 python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
 python manage.py migrate
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Start the Django development server
 python manage.py runserver 0.0.0.0:$PORT
