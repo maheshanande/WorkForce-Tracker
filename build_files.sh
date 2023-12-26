36759e4eb91470fba1d23c417908ef8a8f4bee1b
MAHESH ANANDE <maheshanande6@gmail.com>
2023-12-26 15:58:01 +0530

    added vercel related change

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START "
-python3.11.4 -m pip install -r requirnments.txt
-python3.11.4 manage.py collectstatic --noinput --clear
+python -m pip install -r requirnments.txt
+python manage.py collectstatic --noinput --clear
 echo "BUILD END"
